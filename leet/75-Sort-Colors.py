976a7ce9d0fe9dcde5eab8eec355b88ffa9c53e5
Ali Gomaa <138549566+aligomaa56@users.noreply.github.com>
2024-07-09 03:51:32 +0300

    [75] [Time Beats: 59.34%] [Memory Beats: 21.13%] - LeetPush

--- leet/75-Sort-Colors.py
+++ leet/75-Sort-Colors.py
@@ -4,15 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         \\\
-        low, mid, high = 0, 0, len(nums) - 1
-
-        while mid <= high:
-            if nums[mid] == 0:
-                nums[low], nums[mid] = nums[mid], nums[low]
-                low += 1
-                mid += 1
-            elif nums[mid] == 1:
-                mid += 1
-            else:
-                nums[high], nums[mid] = nums[mid], nums[high]
-                high -= 1
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] > nums[j]:
+                    nums[i], nums[j] = nums[j], nums[i]
