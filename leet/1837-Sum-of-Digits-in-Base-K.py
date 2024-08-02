fac9e17769047e2bb8e74a694562b42bd4035792
Ali Gomaa <138549566+aligomaa56@users.noreply.github.com>
2024-08-03 02:46:09 +0300

    [1837] [Time Beats: 86.78%] [Memory Beats: 5.79%] - LeetPush

---
+++ leet/1837-Sum-of-Digits-in-Base-K.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def sumBase(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        digits = []
+        while n > 0:
+            digits.append(n % k)
+            n = n // k
+        return sum(digits)
