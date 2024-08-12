f8883d2a079a339b092b8ea1585f013c7b143be7
Ali Gomaa <138549566+aligomaa56@users.noreply.github.com>
2024-08-12 03:22:44 +0300

    [1221] [Time Beats: 88.05%] [Memory Beats: 20.95%] - LeetPush

---
+++ leet/1221-Split-a-String-in-Balanced-Strings.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def balancedStringSplit(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        balance = 0
+        count = 0
+        
+        for char in s:
+            if char == 'L':
+                balance += 1
+            else:
+                balance -= 1
+            
+            if balance == 0:
+                count += 1
+        
+        return count
